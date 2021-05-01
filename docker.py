81bb22990090facc3644e022b9b6e9d54358f5fa
Eric Marcantonio <eric11marcantonio@gmail.com>
2021-05-01 15:52:00 -0400

    fix -it to -d

--- docker.py
+++ docker.py
@@ -1,4 +1,5 @@
 import os
 
+os.system("git pull")
 os.system("docker build . -t graphql-node")
-os.system('docker run -it -p 4000:4000 -e "address=10.0.0.230" graphql-node')
+os.system('docker run -d -p 4000:4000 -e "address=10.0.0.230" graphql-node')
